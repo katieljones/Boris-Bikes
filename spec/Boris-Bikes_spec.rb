32f183153e264089e4c4018e771e10d2344cd0cb
Katie Jones <katieljones29@gmail.com>
2020-06-10 21:45:14 +0100

    step 12 raising errors

--- spec/Boris-Bikes_spec.rb
+++ spec/Boris-Bikes_spec.rb
@@ -1,7 +1,17 @@
 require './lib/Boris-Bikes'
 describe DockingStation do
 
-  it {should respond_to(:release_bike)}
+  it 'releases a bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    #subject == DockingStation.new
+    expect(subject.release_bike).to eq bike
+  end
+  
+  it 'raises an error when there are no bikes available' do
+    expect { subject.release_bike }.to raise_error 'No bikes available'
+  end
+
     it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
